787d09ca036944e9539cc4fb1cc7ed3c395b70b3
rtuttle93 <82906399+rtuttle93@users.noreply.github.com>
2021-09-21 06:57:39 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Variables I will need:
+
+var searchButton = $(".searchButton");
+var currentDate = "";
+var searchCityInput = $("#cityInput");
+var apiKey = "9821b0d23b1f66242e790f48ced2591f";
+
+// var searchHistoryList = $('#search-history-list');
+
+// var searchCityButton = $("#search-city-button");
+// var clearHistoryButton = $("#clear-history");
+
+// var currentCity = $("#current-city");
+// var currentTemp = $("#current-temp");
+// var currentHumidity = $("#current-humidity");
+// var currentWindSpeed = $("#current-wind-speed");
+// var UVindex = $("#uv-index");
+
+// var weatherContent = $("#weather-content");
+
+
+// var currentDate = moment().format('L');
+// $("#current-date").text("(" + currentDate + ")");
+
+// console.log(currentDate);
